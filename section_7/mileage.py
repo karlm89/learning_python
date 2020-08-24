10cec15695f25170761dc9d0b6a7f51c58d52feb
Karl Murray <karl@fwdmotion.studio>
2020-08-24 00:19:57 -0500

    Renamed the folders.

---
+++ section_7/mileage.py
@@ -0,0 +1,7 @@
+kilometers = 1.60934
+
+print(f"How many miles would you like to convert? (There are {kilometers} kilometers in a mile")
+
+miles = float(input())/kilometers
+
+print(f"That is { round(miles,2) } kilometers!")
